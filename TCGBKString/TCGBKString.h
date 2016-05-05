89e17fabf18976875fb90c6a6a571d07be8c210b
Tony Tang <tangchang21@gmail.com>
2016-05-05 16:16:46 +0800

    Initial Commit

---
+++ TCGBKString/TCGBKString.h
@@ -0,0 +1,13 @@
+//
+//  TCGBKString.h
+//  TCGBKString
+//
+//  Created by changtang on 16/5/5.
+//  Copyright © 2016年 TCTONY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCGBKString : NSObject
+
+@end
---
+++ TCGBKString/TCGBKString.m
@@ -0,0 +1,13 @@
+//
+//  TCGBKString.m
+//  TCGBKString
+//
+//  Created by changtang on 16/5/5.
+//  Copyright © 2016年 TCTONY. All rights reserved.
+//
+
+#import "TCGBKString.h"
+
+@implementation TCGBKString
+
+@end
